9a0c595249ccfb32c430a739e0894f976a238f6c
IJke Botman <ijke.botman@gmail.com>
2017-11-09 12:25:45 +0100

    Create ChecklistItem class, items array.

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by IJke Botman on 09/11/2017.
+//  Copyright © 2017 Overscope. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
