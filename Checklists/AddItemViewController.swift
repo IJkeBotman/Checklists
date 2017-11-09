800cca2409f9395e5d31999fcebb6032742de76c
IJke Botman <ijke.botman@gmail.com>
2017-11-09 13:18:22 +0100

    Add AddItemViewController

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by IJke Botman on 09/11/2017.
+//  Copyright © 2017 Overscope. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+}
