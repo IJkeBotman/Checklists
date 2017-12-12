66245d8fdadf259d29868c3d0614bd67e4d01943
IJke <ijke@overscope.net>
2017-12-12 11:37:27 +0100

    Local notification in AppDelegate test, print to console when app is active.

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
     let dataModel = DataModel()
@@ -20,7 +21,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let navigationController = window!.rootViewController as! UINavigationController
         let controller = navigationController.viewControllers[0] as! AllListsViewController
         controller.dataModel = dataModel
+        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .sound]) {
+            granted, error in
+            if granted {
+                print("We have permission.")
+            } else {
+                print("Permission denied.")
+            }
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Hello!"
+        content.body = "I am a local notification"
+        content.sound = UNNotificationSound.default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "MyNotifiction", content: content, trigger: trigger)
+        
+        center.add(request)
+        
         return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Received local notification.")
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
